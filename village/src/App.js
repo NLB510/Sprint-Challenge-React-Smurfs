796acdefa3a8ef582dc1de88bbe879b64008bcef
Nate Boyette <nateboyette@icloud.com>
2019-01-25 10:10:03 -0800

    Add buttons edit and delete buttons to smurf items. Add functionality to delete a smurf

--- village/src/App.js
+++ village/src/App.js
@@ -1,75 +1,93 @@
-import React, { Component } from 'react';
-import axios from "axios"
+import React, { Component } from "react";
+import axios from "axios";
 import { Route, Link } from "react-router-dom";
 
 import styled from "styled-components";
 
-import './App.css';
+import "./App.css";
 
-import SmurfForm from './components/SmurfForm';
-import Smurfs from './components/Smurfs';
-import NavBar from "./components/NavBar"
+import SmurfForm from "./components/SmurfForm";
+import Smurfs from "./components/Smurfs";
+import NavBar from "./components/NavBar";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      smurfs: [],
+      smurfs: []
     };
   }
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
   componentDidMount() {
-    axios.get(`http://localhost:3333/smurfs`)
+    axios
+      .get(`http://localhost:3333/smurfs`)
       .then(res => {
         this.setState({
           smurfs: res.data
-        })
+        });
       })
-      .catch(err => console.log(err))
+      .catch(err => console.log(err));
   }
 
-
-  postSmurf = (smurf) => {
-    axios.post(`http://localhost:3333/smurfs`, smurf)
+  postSmurf = smurf => {
+    axios
+      .post(`http://localhost:3333/smurfs`, smurf)
       .then(res => {
-        console.log(res.data)
+        console.log(res.data);
         this.setState({
           smurfs: res.data
-        })
+        });
+        this.props.history.push("/");
       })
-      .catch(err => console.log(err))
-  }
+      .catch(err => console.log(err));
+      
+  };
 
+  deleteSmurf = (e, smurfId) => {
+    axios
+      .delete(`http://localhost:3333/smurfs/${smurfId}`)
+      .then(res => {
+        this.setState({
+          smurfs: res.data
+        });
+      })
+      .catch(err => console.log(err));
+  };
 
   render() {
-
     return (
       <div className="App">
-        
-          <NavBar />
-        
-        
-        
-        <div className='link-container'>
-          <Link to="/smurf-form" className="app-link">Add A Smurf</Link>
-          <Link to="/" className="app-link">Smurf List</Link>
-        </div>
-        
-        <Route exact path="/" render={props => <Smurfs {...props} smurfs={this.state.smurfs} />} />
-        <Route path="/smurf-form" render={props => <SmurfForm {...props} postSmurf={this.postSmurf} /> } />
+        <NavBar />
 
+        <div className="link-container">
+          <Link to="/smurf-form" className="app-link">
+            Add A Smurf
+          </Link>
+          <Link to="/" className="app-link">
+            Smurf List
+          </Link>
+        </div>
+
+        <Route
+          exact
+          path="/"
+          render={props => (
+            <Smurfs
+              {...props}
+              smurfs={this.state.smurfs}
+              deleteSmurf={this.deleteSmurf}
+            />
+          )}
+        />
+        <Route
+          path="/smurf-form"
+          render={props => <SmurfForm {...props} postSmurf={this.postSmurf} />}
+        />
       </div>
     );
   }
 }
-
-
-
-
-
-
-
 
 export default App;
--- village/src/components/Smurf.js
+++ village/src/components/Smurf.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import styled from "styled-components"
+import {Button} from "reactstrap"
 
 const Smurf = props => {
   return (
-    <div className="Smurf">
+    <SmurfDiv>
       <h3>{props.name}</h3>
       <strong>{props.height} tall</strong>
       <p>{props.age} smurf years old</p>
-    </div>
+      <ButtonContainer>
+        <Button color="secondary" size="sm">Edit</Button>
+        <Button color="danger" size="sm" onClick={(e) => props.deleteSmurf(e, props.id)}>Delete</Button>
+      </ButtonContainer>
+    </SmurfDiv>
   );
 };
 
@@ -15,6 +21,28 @@ Smurf.defaultProps = {
   height: '',
   age: ''
 };
+
+
+
+/* 
+=== Smurf Component Styles
+*/
+
+const SmurfDiv = styled.div`
+  border: 1px solid lightgrey;
+  width: 20%;
+  padding: 2% 1%;
+  margin: 2% auto;
+
+  border-radius: 5px;
+`
+
+const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+`
+
+
 
 export default Smurf;
 
